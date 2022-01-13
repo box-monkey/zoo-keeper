67711051c2becee5471146bc8a1b70bcea43b34f
box-monkey <jr.gasparin15@gmail.com>
2022-01-13 10:13:01 -0500

    finished 2

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require('express');
-const { animals } = require('./data/animals');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const { animals } = require("./data/animals");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// ------------------- parse incoming data -------------------
+
+// The express.urlencoded({extended: true}) method is a method built into Express.js. It takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object. The extended: true option set inside the method call informs our server that there may be sub-array data nested in it as well, so it needs to look as deep into the POST data as possible to parse all of the data correctly.
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+// ----------------------- quries and filter paramters ------------------
 
 // this function will take req.query as an argument, filter through animals and return a new filtered array by either diet, species or name.
 function filterByQuery(query, animalsArray) {
@@ -46,21 +59,127 @@ function filterByQuery(query, animalsArray) {
       (animal) => animal.name === query.name
     );
   }
-  // return the filtered results:
   return filteredResults;
 }
 
 // takes id + array, returns single animal object.
 function findById(id, animalsArray) {
-  const result = animalsArray.filter(animal => animal.id === id)[0];
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
 }
 
+// We are going to execute this function from within the app.post() route's callback function and when we do, it'll take the new animal data and add it to the animalsArray we passed in, and then write the new array data to animals.json. After saving it, we'll send the data back to the route's callback function so it can finally respond to the request. Before we do that, however, we need to add something to the new animal's data.
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+// -------------------------- routes section -------------------------
 /*  
     The first is that the get() method requires two arguments. The first is a string that describes the route the client will have to fetch from. The second is a callback function that will execute every time that route is accessed with a GET request.
 
     The second takeaway is that we are using the send() method from the res parameter (short for response) to send the string Hello! to our client.
 */
+
+app.get("/api/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { animals } = require('./data/animals');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === 'string') {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+    personalityTraitsArray.forEach(trait => {
+      filteredResults = filteredResults.filter(
+        animal => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+  }
+  return filteredResults;
+}
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
@@ -79,6 +198,39 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
+
+  } else {
+    res.send(404);
+  }
+});
+
+// creates
+app.post("/api/animals", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+// -----------   PORT / server ---------------
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
